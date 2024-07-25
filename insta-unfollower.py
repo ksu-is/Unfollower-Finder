d792ec5fbc11a5280449e68fe082ac68506cb979
ryleetate <rtate26@students.kennesaw.edu>
2024-07-25 14:38:24 -0400

    Update insta-unfollower.py
    seperate usernames by spaces, not commas

--- insta-unfollower.py
+++ insta-unfollower.py
@@ -12,7 +12,7 @@ class InstagramUnfollower:
         
     def load_list_from_input(self, prompt):
         print(prompt)
-        return input().split(',')
+        return input().split()
 
     def get_unfollowers(self):
         followers_set = set(self.followers)
@@ -20,8 +20,8 @@ class InstagramUnfollower:
         return unfollowers
 
      def run(self):
-        self.followers = self.load_list_from_input("Please enter your followers, separated by commas:")
-        self.following = self.load_list_from_input("Please enter the accounts you are following, separated by commas:")
+        self.followers = self.load_list_from_input("Please enter your followers, separated by spaces:")
+        self.following = self.load_list_from_input("Please enter the accounts you are following, separated by spaces:")
 
         unfollowers = self.get_unfollowers()
 
